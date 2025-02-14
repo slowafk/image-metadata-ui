3b5920f55a9b440593d282c409374390af33adb8
slow <slowemperor.eth@gmail.com>
2025-02-14 17:42:54 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import { ImageUploadWithMetadata } from './components/ImageUploadWithMetadata'; 
+ 
+function App() { 
+  return ( 
+    <div className="container mx-auto p-4"> 
+      <ImageUploadWithMetadata /> 
+    </div> 
+  ); 
+} 
+ 
+export default App; 
